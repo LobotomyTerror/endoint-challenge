4578aa3d6c8dffad951635f6faa6d5b7d07cd0aa
LobotomyTerror <dfishbein90@gmail.com>
2026-04-24 20:58:19 -0600

    moved models to separate to clean up the work space

---
+++ app/models.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class Cases(BaseModel):
+    challenge_id: str
+    schema_version: int
+    generated_at: str
+    cases: List[CaseDetails]
+
+
+class CaseDetails(BaseModel):
+    case_id: str
+    patient_id: str
+    patient_name: str
+    current_study: CurrentStudy
+    prior_studies: List[PriorStudies]
+
+
+class CurrentStudy(BaseModel):
+    study_id: str
+    study_description: str
+    study_date: str
+
+
+class PriorStudies(BaseModel):
+    study_id: str
+    study_description: str
+    study_date: str
+
+
+class Response(BaseModel):
+    predictions: List[PredicitionResponse]
+
+
+class PredicitionResponse(BaseModel):
+    case_id: str
+    study_id: str
+    predicted_is_relevant: bool
