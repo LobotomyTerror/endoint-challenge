4c9af44e2aa685600d83dcc11cc933b56979d8af
LobotomyTerror <dfishbein90@gmail.com>
2026-04-24 22:19:25 -0600

    updated endpoint to return desired results'

--- app/main.py
+++ app/main.py
@@ -1,49 +1,22 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
-from typing import List, Dict
+from typing import Dict
+import json
 
-import resources
-
-class Cases(BaseModel):
-    challenge_id: str
-    schema_version: int
-    generated_at: str
-    cases: List[CaseDetails]
-
-class CaseDetails(BaseModel):
-    case_id: str
-    patient_id: str
-    patient_name: str
-    current_study: CurrentStudy
-    prior_studies: List[PriorStudies]
-
-class CurrentStudy(BaseModel):
-    study_id: str
-    study_description: str
-    study_date: str
-
-class PriorStudies(BaseModel):
-    study_id: str
-    study_description: str
-    study_date: str
-
-class Response(BaseModel):
-    predictions: List[PredicitionResponse]
-
-class PredicitionResponse(BaseModel):
-    case_id: str
-    study_id: str
-    predicted_is_relevant: bool
+from app import resources, models
 
 app = FastAPI()
-
 
 
 @app.get("/")
 def read_root() -> Dict[str, str]:
     return { "Hello": "World" }
 
-@app.post("/challenge-response/")
-def challenge_response(cases: Cases) -> None:
-    ...
+
+@app.post("/challenge-response/", response_model=models.Response)
+def challenge_response(cases: models.Cases):
+    results = []
+    for case in cases.cases:
+        results.extend(resources.match_prior_studies(case.case_id, case.prior_studies))
+
+    return models.Response(predictions=results)
